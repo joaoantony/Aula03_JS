b4ce3ff954b5f27676dbfa23c9187bb4897c59f1
joaoantony <antonyjoao50@gmail.com>
2026-04-15 11:00:30 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -1,1 +1,58 @@
+/* Criar uma variávl */
+let nome = "Sabrina";
+let altura = 1.80;
 
+/* Concatenação de Strings */
+console.log(nome + "possui" + altura + "de altura.");
+
+/*Interpolação de strings */
+console.log(`${nome} possui ${altura} de altura.`);
+
+let numero = 10;
+let texto = "10";
+/*Comparação com igualdade (==) */
+console.log(numero == texto);
+
+/*Comparação com igualdade estrita (===) */
+console.log(numero === texto);
+
+/*Operador Lógico (E) -- Representação: && */
+/*Operador Lógico (OU) - Representação: `*/
+let media = 5.5;
+
+if(media>=6 && media<=10){
+    console.log("Aluno aprovado");
+}else if(media=4 && media<6){
+    console.log("Aluno de Exame");
+}else{
+    console.log("Aluno Reprovado");
+}
+
+let valor = 3;
+
+switch(valor){
+    case 1:
+        console.log("O valor é 1");
+        break;
+    case 2:
+        console.log("O valor é 2");
+        break;
+    case 3:
+        console.log("O valor é 3");
+        break;
+    case 4:
+        console.log("O valor é 4");
+        break;
+    default:
+        console.log("Opção inválida");
+        break;
+
+}
+
+const readline = require('realine-sync');
+let nome1 = readline.question('Digite seu nome: ');
+
+console.log(nome1);
+
+/* Cenas dos próximos capítulos 
+Laço de repetição: for(), while() e do...while() */
